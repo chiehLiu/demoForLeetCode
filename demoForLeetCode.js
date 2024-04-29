1d25c92121523ea1c7000d59458779580acdafb7
chieh <chieh@25sprout.com>
2024-04-29 20:36:35 +0800

    Implement Heap-base Priority Queue

--- demoForLeetCode.js
+++ demoForLeetCode.js
@@ -1,21 +1,28 @@
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
 
-// MaxHeap
-class Heap {
+class PriorityQueue {
   constructor() {
     this.heapElements = [];
   }
 
-  insert(value) {
-    this.heapElements.push(value);
+  insert(value, priority) {
+    const newNode = new Node(value, priority);
+
+    this.heapElements.push(newNode);
     let currentElementIndex = this.heapElements.length - 1;
     let parentElementIndex = Math.floor((currentElementIndex - 1) / 2);
 
     while (
       parentElementIndex >= 0 &&
-      this.heapElements[currentElementIndex] > this.heapElements[parentElementIndex]
+      this.heapElements[currentElementIndex].priority > this.heapElements[parentElementIndex].priority
     ) {
       const parentElement = this.heapElements[parentElementIndex];
-      this.heapElements[parentElementIndex] = this.heapElements[currentElementIndex];
+      this.heapElements[parentElementIndex] = newNode;
       this.heapElements[currentElementIndex] = parentElement;
       currentElementIndex = parentElementIndex;
       parentElementIndex = Math.floor((currentElementIndex - 1) / 2);
@@ -31,34 +38,32 @@ class Heap {
       return this.heapElements.pop();
     }
 
+    const topElement = this.heapElements[0];
+
     this.heapElements[0] = this.heapElements.pop();
 
     let currentElementIndex = 0;
     let leftChildIndex = 2 * currentElementIndex + 1;
     let rightChildIndex = 2 * currentElementIndex + 2;
-    let childElementIndex = this.heapElements[leftChildIndex] > this.heapElements[rightChildIndex] ? leftChildIndex : rightChildIndex;
+    let childElementIndex = this.heapElements[rightChildIndex] && this.heapElements[rightChildIndex].priority >= this.heapElements[leftChildIndex].priority ? rightChildIndex : leftChildIndex;
   
-    while (this.heapElements[childElementIndex] && this.heapElements[currentElementIndex] <= this.heapElements[childElementIndex]) {
+    while (this.heapElements[childElementIndex] && this.heapElements[currentElementIndex].priority <= this.heapElements[childElementIndex].priority) {
       const currentNode = this.heapElements[currentElementIndex];
       const childNode = this.heapElements[childElementIndex];
       this.heapElements[childElementIndex] = currentNode;
       this.heapElements[currentElementIndex] = childNode;
     }
+  
+    return topElement;
   }
 }
 
-const heap = new Heap();
+const heap = new PriorityQueue();
 
+heap.insert("Clean the room", 1);
+heap.insert("Wash the dishes", 2);
+heap.insert("Buy groceries", 3);
 
-heap.insert(40);
-heap.insert(101);
-heap.insert(197);
-heap.insert(12);
-heap.insert(15);
-heap.insert(85);
-heap.insert(250);
-
-heap.process();
 heap.process();
 
 console.log("🚀 ~ heap:", heap)
