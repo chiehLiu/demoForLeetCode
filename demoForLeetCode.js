5774f716f1c87737673237ab7b09063369560afd
chieh <chieh@25sprout.com>
2024-05-01 14:58:19 +0800

    Add basic Graph

--- demoForLeetCode.js
+++ demoForLeetCode.js
@@ -1,60 +1,30 @@
-class Node {
-  constructor (value, priority) {
-    this.value = value;
-    this.next = null;
-    this.priority = priority;
-  }
-}
-
-class PriorityQueue {
+class Graph {
   constructor () {
-    this.first = null;
+    this.nodes = {};
+    this.edges = {};
   }
 
-  // insert a new node
-  insert (value, priority) {
-    const newNode = new Node(value, priority);
+  addNode (identifier, value) {
+    this.nodes[identifier] = value;
+  }
 
-    // if the queue is empty or the new node has higher priority than the first node
-    if (!this.first || priority > this.first.priority) {
-
-      // the new node points to the first node
-      newNode.next = this.first;
-
-      // update the first node
-      this.first = newNode;
+  addEdge (startNode, endNode) {
+    if (this.edges[startNode] && this.edges[startNode].indexOf(endNode) === -1) {
+      this.edges[startNode].push(endNode);
     } else {
-      let currentNode = this.first;
-
-      // find the right place to insert the new node, based on the priority value of the nodes in the queue
-      while (currentNode.next && priority < currentNode.next.priority) {
-        // move to the next node
-        currentNode = currentNode.next;
-      }
-      
-      // insert the new node
-      newNode.next = currentNode.next;
-      // update the next node of the current node
-      currentNode.next = newNode;
+      this.edges[startNode] = [endNode];
     }
   }
-
-  // return and remove the first node
-  process () {
-    const first = this.first;
-    this.first = this.first.next;
-    return first;
-  }
 }
 
-const queue = new PriorityQueue();
+const graph = new Graph();
 
-queue.insert('clean', 1);
-queue.insert('do tax', 100);
-queue.insert('learn to code', 1000);
+graph.addNode(1, 'Max');
+graph.addNode(2, 'Manuel');
+graph.addNode(3, 'Jules');
 
-console.log(queue);
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(3, 2);
 
-console.log(queue.process());
-console.log(queue.process());
-console.log(queue.process());
+console.log(graph);
